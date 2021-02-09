00307c733b9a1fbc3d7197a8976f69c6fccc44df
Chelsea <chelsea.e.rolland@gmail.com>
2021-02-09 10:50:53 -0600

    completed the block letters project

---
+++ initials.py
@@ -0,0 +1,25 @@
+#Chelsea Rolland
+#Favorite Colors are Vermillion, Teal, Jade
+
+first_inital = """ 
+ CCC
+C   C
+C
+C
+C
+C   C
+ CCC
+"""
+
+second_inital = """
+RRRR
+R    R 
+R    R
+RRRR 
+R  R
+R   R
+R    R
+"""
+
+print(first_inital)
+print(second_inital)
--- intro.py
+++ intro.py
@@ -17,5 +17,31 @@ string3 = "sank at sunset to a light breeze; "
 string4 = "the soft air just ruffled the water and "
 string5 = "caused a pleasant motion among the trees as we approached the shore, "
 string6 = "from which it wafted the most delightful scent of flowers and hay."
+num = 17
 
-print(string1 + string2 + string3 + string4 + string5 + string6)
+print(string1 + string2 + string3 + string4 + string5 + string6 + str(num))
+
+#operator equals notation
+total_price = 0
+new_sneakers = 50.00
+total_price += new_sneakers
+nice_sweater = 39.00
+fun_books = 20.00
+total_price += nice_sweater + fun_books
+print("The total price is", total_price)
+
+#Multi level strings
+poem = """ JANUARY
+Delightful display
+Snowdrops bow their pure white heads
+To the sun's glory.
+"""
+print(poem)
+
+#Recap
+my_age = 27
+half_my_age = 16.5
+greeting = "Hello there fellow dude"
+name = "Chelsea"
+greeting_with_name = greeting + ", " + name
+print(greeting_with_name)
