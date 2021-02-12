9dc4d209f9c21b30e14bd536b6fdf3aed929ba5e
Chelsea <chelsea.e.rolland@gmail.com>
2021-02-12 17:56:34 -0600

    completed the modifications of lists

--- intro.py
+++ intro.py
@@ -260,3 +260,65 @@ all_customers = zip(ids, first_names, all_ages)
   #Answer: [(0, 'Ainsley', 32), (1, 'Ben', 41), (2, 'Chani', 29), (3, 'Depak', 42)]
 print(list(all_customers))
 
+#Operations on Lists
+
+#Length
+print(len(first_names))
+
+list1 = range(2, 20, 3)
+list1_len = len(list1)
+print(list1_len)
+
+#Selecting Elements #1
+employees = ['Michael', 'Dwight', 'Jim', 'Pam', 'Ryan', 'Andy', 'Robert']
+index4 = employees[4]
+print(len(employees))
+print(employees[6])
+
+#Selecting Elements #2
+shopping_list = ['eggs', 'butter', 'milk', 'cucumbers', 'juice', 'cereal']
+print(len(shopping_list))
+last_element = shopping_list[-1]
+element5 = shopping_list[5]
+print(last_element, element5)
+
+#Slicing
+suitcase = ['shirt', 'shirt', 'pants', 'pants', 'pajamas', 'books']
+beginning = suitcase[0:2]  # ['shirt','shirt']
+beginning = suitcase[0:4]  # ['shirt', 'shirt', 'pants', 'pants']
+print(beginning)
+middle = suitcase[2:4]  # ['pants','pants']
+print(middle)
+
+#Slicing 2
+suitcase = ['shirt', 'shirt', 'pants', 'pants', 'pajamas', 'books']
+start = suitcase[:3]  # ['shirt','shirt','pants']
+end = suitcase[4:]  # ['pajamas','books']
+
+#Counting
+votes = ['Jake', 'Jake', 'Laurie', 'Laurie', 'Laurie', 'Jake', 'Jake', 'Jake', 'Laurie', 'Cassie',
+'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie', 'Cassie', 'Jake', 'Jake', 'Cassie', 'Laurie']
+jake_votes = votes.count("Jake")
+print(jake_votes)
+
+#Sort
+### Exercise 1 & 2 ###
+addresses = ['221 B Baker St.', '42 Wallaby Way', '12 Grimmauld Place','742 Evergreen Terrace', '1600 Pennsylvania Ave', '10 Downing St.']
+# Sort addresses here:
+addresses.sort()
+print(addresses)
+### Exercise 3 ###
+names = ['Ron', 'Hermione', 'Harry', 'Albus', 'Sirius']
+names.sort()
+print(names)
+### Exercise 4 ###
+cities = ['London', 'Paris', 'Rome', 'Los Angeles', 'New York']
+sorted_cities = cities.sort()
+print(sorted_cities)
+
+#Sorting 2
+games = ['Portal', 'Minecraft', 'Pacman', 'Tetris', 'The Sims', 'Pokemon']
+games_sorted = sorted(games)
+print(games_sorted)
+
+
