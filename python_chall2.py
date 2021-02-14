313042a3b1ba28f9e9414233c6e92a8a9851441f
Chelsea <chelsea.e.rolland@gmail.com>
2021-02-14 16:15:30 -0600

    completed the q2 of chall

--- python_chall2.py
+++ python_chall2.py
@@ -7,4 +7,15 @@ def append_size(lst):
 print("1.1 Test: ", append_size([28, 15, 38]))  #[28,15,38,3]
 print("1.2 Test: ", append_size([23, 42, 108])) #[23,42,108,3]
 
-#2. 
+#2. Append Sum
+def append_sum(lst):
+    lst.append(lst[-1] + lst[-2])
+    lst.append(lst[-1] + lst[-2])
+    lst.append(lst[-1] + lst[-2])
+    return lst
+
+#2. Tests
+print("2.1 Test: ", append_sum([1,1,2]))
+
+#3. Larger List
+
