01283a41ec5c2a71b24dec5ec7368690e368a203
Chelsea <chelsea.e.rolland@gmail.com>
2021-02-17 18:17:03 -0600

    completed the list python challenges

--- python_chall2.py
+++ python_chall2.py
@@ -26,7 +26,7 @@ def larger_list(lst1, lst2):
         return lst2[-1]
 
 #3. Tests
-print(larger_list([4,10,2,5],[-10,2,5,10]))  #5
+print("3.1 Tests: ",larger_list([4,10,2,5],[-10,2,5,10]))  #5
 
 #4. More than N
 def more_than_n(lst, item, n):
@@ -36,10 +36,65 @@ def more_than_n(lst, item, n):
         return False
 
 #4. Tests
-print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3)) #True
+print("4.1 Tests: ",more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3)) #True
 
 #5. Combine Sort
 def combine_sort(lst1, lst2):
     return sorted(lst1 + lst2)
 
-print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10])) #[-10, 2, 2, 4, 5, 5, 10, 10]
+print("5.1 Tests: ",combine_sort([4, 10, 2, 5], [-10, 2, 5, 10])) #[-10, 2, 2, 4, 5, 5, 10, 10]
+
+#6. Every Three Numbers
+def every_three_nums(start):
+    if start >= 100:
+        return []
+    else:
+        return list(range(start,101,3))
+
+#6. Tests
+print("6.1 Test: ",every_three_nums(91)) #[91,94,97,100]
+
+#7. Remove Middle
+def remove_middle(lst, start, end):
+    return lst[:start] + lst[end+1:]
+
+#7. Tests
+print("7.1 Tests: ",remove_middle([4, 8, 15, 16, 23, 42], 1, 3)) #[4, 23, 42]
+
+#8. More Frequent Item
+def more_frequent_item(lst, item1, item2):
+    if lst.count(item1) >= lst.count(item2):
+        return item1
+    else:
+        return item2
+
+#8. Tests
+print("8.1 Tests: ",more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
+
+#9. Double Index
+def double_index(lst, index):
+    if index >= len(lst):
+        return lst
+    else:
+        new_lst = lst[0:index]
+        new_lst.append(lst[index]*2)
+        new_lst = new_lst + lst[index+1:]
+        return new_lst
+
+#9. Tests
+print("9.1 Tests: ", double_index([1, 2, 3, 4], 2))
+print("9.2 Tests: ",double_index([3, 8, -10, 12], 2))
+
+#10. Middle Item
+def middle_element(lst):
+    med_index = int(len(lst) / 2)
+    if len(lst) % 2 == 0:
+        sum = lst[med_index] + lst[med_index - 1]
+        return sum / 2
+    else:
+        return lst[med_index]        
+
+#10. Tests
+print(middle_element([5, 2, -10, -4, 4, 5]))
+
+
