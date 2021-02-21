20377f4e753f02fcf60e340c2999bde9bf6a9dbc
Chelsea <chelsea.e.rolland@gmail.com>
2021-02-21 10:37:21 -0600

    still working on the n digit armstrong encountering a problem now will try to resolve the zerodivionerror

--- armstrong_num.py
+++ armstrong_num.py
@@ -15,10 +15,41 @@ def three_armstrong_digits(num):
         temp //= 10
 
     if num == sum:
-        print(num, "is an Armstrong number")
+        print(num, " is an Armstrong number")
         return "True"
     else:
-        print(num, "is not an Armstrong number")
+        print(num, " is not an Armstrong number")
         return "False"
 
 print("3 Digit Armstrong Number: ",three_armstrong_digits(num))
+
+
+#Checking Armstrong Num for n digits
+#---------------------------------------
+def n_digits_armstrong(num):
+    # Changed num variable to string,
+    # and calculated the length (number of digits)
+    order = len(str(num))
+    #initalize the sum
+    sum = 0
+    #find the sum of the order(# of digits) of each digit
+    temp = num
+    while temp > 0:
+        remainder = temp % 0
+        sum += remainder ** order
+        temp //= 10
+    
+    if num == sum:
+        print(num, " is an Armstrong Number")
+        return "True"
+    else:
+        print(num, " is not an Armstrong Number")
+        return "False"
+
+#print("N Digits Armstrong Number: ", n_digits_armstrong(num))
+
+if len(str(num)) == 3:
+    print("3 Digit Armstrong Number: ", three_armstrong_digits(num))
+else:
+    print("N Digits Armstrong Number: ", n_digits_armstrong(num))
+
