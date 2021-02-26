949c3d75564a5cb339df8b3d90df5cd9da4eb262
Chelsea <chelsea.e.rolland@gmail.com>
2021-02-25 21:47:05 -0600

    still working on the string methods and have reached the review and will complete the review tomorrow

--- intro.py
+++ intro.py
@@ -549,6 +549,7 @@ poem_author_fixed = poem_author.upper()
 print(poem_author)
 print("String Methods - Upper: ", poem_author_fixed)
 
+#Splitting Strings
 line_one = "The sky has given over"
 line_one_words = line_one.split()
 print("String Method - Split: ", line_one_words)
@@ -563,3 +564,78 @@ for name in author_names:
     author_last_names.append(name.split()[-1])
 
 print("String Methods - Split Delimiters 1.2: ", author_last_names)
+
+spring_storm_text = \
+"""The sky has given over 
+its bitterness. 
+Out of the dark change 
+all day long 
+rain falls and falls 
+as if it would never end. 
+Still the snow keeps 
+its hold on the ground. 
+But water, water 
+from a thousand runnels! 
+It collects swiftly, 
+dappled with black 
+cuts a way for itself 
+through green ice in the gutters. 
+Drop after drop it falls 
+from the withered grass-stems 
+of the overhanging embankment."""
+
+spring_storm_lines = spring_storm_text.split("\n")
+print("String Splitting 3: ",spring_storm_lines)
+
+#Joining Strings
+reapers_line_one_words = ["Black", "reapers", "with", "the", "sound", "of", "steel", "on", "stones"]
+
+reapers_line_one = " ".join(reapers_line_one_words)
+print("Joining: ", reapers_line_one)
+
+winter_trees_lines = ['All the complicated details', 'of the attiring and', 'the disattiring are completed!', 'A liquid moon', 'moves gently among', 'the long branches.', 'Thus having prepared their buds', 'against a sure winter', 'the wise trees', 'stand sleeping in the cold.']
+
+winter_trees_full = "\n".join(winter_trees_lines)
+print("Joining String 2: ", winter_trees_full)
+
+#.strip() method
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
+
+love_maybe_lines_stripped = []
+for line in love_maybe_lines:
+    love_maybe_lines_stripped.append(line.strip())
+print(".strip method 1.1: ", love_maybe_lines_stripped)
+
+love_maybe_full = "\n".join(love_maybe_lines_stripped)
+print(".strip method 1.2: ", love_maybe_full)
+
+#Replace
+toomer_bio = \
+    """
+Nathan Pinchback Tomer, who adopted the name Jean Tomer early in his literary career, was born in Washington, D.C. in 1894. Jean is the son of Nathan Tomer was a mixed-race freedman, born into slavery in 1839 in Chatham County, North Carolina. Jean Tomer is most well known for his first book Cane, which vividly portrays the life of African-Americans in southern farmlands.
+"""
+
+toomer_bio_fixed = toomer_bio.replace("Tomer","Toomer")
+print("replace method: ", toomer_bio_fixed)
+
+#.find() method
+god_wills_it_line_one = "The very earth will disown you"
+
+disown_placement = god_wills_it_line_one.find("disown")
+print("find() method: ", disown_placement)
+
+#.format() method
+def poem_title_card(title, poet):
+    return "The poem \"{}\" is written by {}.".format(title, poet)
+
+print("format() method: ", poem_title_card("I Hear America Singing", "Walt Whitman"))
+
+def poem_description(publishing_date, author, title, original_work):
+    poem_desc = "The poem {title} by {author} was originally published in {original_work} in {publishing_date}.".format(publishing_date = publishing_date, author = author, title = title, original_work = original_work)
+    return poem_desc
+
+
+my_beard_description = poem_description(
+    author="Shel Silverstein", title="My Beard", original_work="Where the Sidewalk Ends", publishing_date="1974")
+
+print("format() method 2: ", my_beard_description)
