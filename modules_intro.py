1ad33d3ccf9086a41aaae1aa0472a2807c564e71
Chelsea <chelsea.e.rolland@gmail.com>
2021-03-02 16:44:06 -0600

    completed the review of datetime

--- modules_intro.py
+++ modules_intro.py
@@ -19,7 +19,8 @@ print("Decimal 2: ", two_decimal_points)
 four_decimal_points = Decimal("0.53") * Decimal("0.65")
 print("Decimal 4: ", four_decimal_points)
 
-#DateTime In more Depth
+#DateTime In more Depth --> Review the python docs for more info
+
 #datetime(year, month, day, hour, minute, second)
 old_day = datetime(1575, 7, 13, 13, 9, 57)
 print(old_day.year)
@@ -29,3 +30,15 @@ print(old_day.weekday())
 print(datetime(2021,1,1) - datetime(2017,1,1))
 print(datetime.now() - datetime(2018,1,1))
 
+#Parsing a Date
+parsed_date = datetime.strptime("Jan 05, 2009", "%b %d, %Y")
+print(parsed_date.month)
+print(parsed_date.year)
+
+parsed_date = datetime.strptime("Jul 13, 2021", "%b %d, %Y")
+print(parsed_date.month)
+print(parsed_date.year)
+
+#Rendering a Date as a string
+date_string = datetime.strftime(datetime.now(), "%b %d, %Y")
+print(date_string)
