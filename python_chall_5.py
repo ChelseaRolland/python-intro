c921d6240572c1560ab21e3fb004726aaa165601
Chelsea <chelsea.e.rolland@gmail.com>
2021-03-04 12:34:20 -0600

    completed q.1 and q.2

---
+++ python_chall_5.py
@@ -0,0 +1,50 @@
+#1. Sum Values
+def sum_values(my_dictionary):
+    total = 0
+    for num in my_dictionary.values():
+        total += num
+    return total
+
+#1 - Tests
+print("Chall 1.1: ", sum_values({"milk":5, "eggs":2, "flour": 3}))
+print("Chall 1.2: ", sum_values({10:1, 100:2, 1000:3}))
+
+#2. Even Keys
+def sum_even_keys(my_dictionary):
+    total = 0
+    for even in my_dictionary.keys():
+        if even % 2 == 0:
+            total += my_dictionary[even]
+    return total
+
+#2 - Tests
+print("Chall 2.1: ", sum_even_keys({1:5, 2:2, 3:3}))
+print("Chall 2.2: ", sum_even_keys({10:1, 100:2, 1000:3}))
+
+#3.
+
+#3 - Tests
+
+#4.
+
+#4 - Tests
+
+#5.
+
+#5 - Tests
+
+#6.
+
+#6 - Tests
+
+#7.
+
+#7 - Tests
+
+#8.
+
+#8 - Tests
+
+#9.
+
+#9 - Tests
