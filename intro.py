cb55797f5374d60e1c4062835291d75824fde627
Chelsea <chelsea.e.rolland@gmail.com>
2021-03-06 00:06:20 -0600

    completed reading different types of csv file, creating a csv file, and now onto reading a json file

--- intro.py
+++ intro.py
@@ -822,6 +822,7 @@ gen_file.close()
 with open("users.csv") as users_file:
     print(users_file.read())
 
+#Read a CSV File
 with open("users.csv", newline="") as users_csv:
     list_of_email_addresses = []
     #Converts the lines in csv file to dictionaries
@@ -829,6 +830,25 @@ with open("users.csv", newline="") as users_csv:
     for user in user_reader:
         list_of_email_addresses.append(user["Email"])
     print(list_of_email_addresses)
+
+#Reading a different type of CSV file
+with open("addresses.csv", newline="") as addresses_csv:
+    addresses_reader = csv.DictReader(addresses_csv, delimiter = ";")
+    for row in addresses_reader:
+        print(row["Name"])
+        print(row["Address"])
+        #print(row["Address"].strip("\n")) --> didn't work will see why
+        print(row["Telephone"])
+
+#Writing a CSV file
+access_log = [{'time': '08:39:37', 'limit': 844404, 'address': '1.227.124.181'}, {'time': '13:13:35', 'limit': 543871, 'address': '198.51.139.193'}, {'time': '19:40:45', 'limit': 3021, 'address': '172.1.254.208'}, {'time': '18:57:16', 'limit': 67031769, 'address': '172.58.247.219'}, {'time': '21:17:13', 'limit': 9083, 'address': '124.144.20.113'}, {'time': '23:34:17', 'limit': 65913, 'address': '203.236.149.220'}, {'time': '13:58:05', 'limit': 1541474, 'address': '192.52.206.76'}, {'time': '10:52:00', 'limit': 11465607, 'address': '104.47.149.93'}, {'time': '14:56:12', 'limit': 109, 'address': '192.31.185.7'}, {'time': '18:56:35', 'limit': 6207, 'address': '2.228.164.197'}]
+fields = ['time', 'address', 'limit']
+
+with open("logger.csv", "w") as logger_csv:
+    log_writer = csv.DictWriter(logger_csv, fieldnames=fields)
+    log_writer.writeheader()
+    for row in access_log:
+        log_writer.writerow(row)
 
 
 
