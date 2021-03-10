c9c7dfa439c80052bf78fa9d32ea6750254c4390
Chelsea <chelsea.e.rolland@gmail.com>
2021-03-10 11:40:19 -0600

    working through the exercise with printing out the string for the menus

---
+++ basta_fazoolin.py
@@ -0,0 +1,36 @@
+import datetime
+
+class Menu:
+    def __init__(self, name, items, start_time, end_time):
+        self.name = name
+        self.items = items
+        self.start_time =  start_time
+        self.end_time = end_time
+    
+    def __repr__(self):
+        return "{} menu available from {} to {}".format(self.name, self.start_time, self.end_time)
+
+    def calculate_bill(self, order):
+
+
+brunch = Menu("brunch", {
+    'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50
+}, datetime.time(11), datetime.time(16))
+
+early_bird = Menu("early_bird", {
+    'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00,
+}, datetime.time(15), datetime.time(18))
+
+dinner = Menu("dinner", {
+    'crostini with eggplant caponata': 13.00, 'ceaser salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00,
+}, datetime.time(17), datetime.time(23))
+
+kids = Menu("kids", {
+    'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00
+}, datetime.time(11), datetime.time(21))
+
+print(brunch)
+print(early_bird)
+print(dinner)
+print(kids)
+
