1fbdc4328964daa3e93143e2c33a1b15d4951518
Chelsea <chelsea.e.rolland@gmail.com>
2021-03-17 19:44:19 -0500

    working through an issue with the calculate drivers cost function

--- nile_shipping.py
+++ nile_shipping.py
@@ -4,10 +4,10 @@ from test import test_function
 def calculate_shipping_cost(from_coords, to_coords, shipping_type="Overnight"):
     from_lat, from_long = from_coords
     to_lat, to_long = to_coords
-    distance = get_distance(from_lat, from_long, to_lat, to_long)
-    #distance = get_distance(*from_coords, *to_coords)
-    #shipping_rate = SHIPPING_PRICES.get(shipping_type)
-    shipping_rate = SHIPPING_PRICES[shipping_type]
+    #distance = get_distance(from_lat, from_long, to_lat, to_long)
+    distance = get_distance(*from_coords, *to_coords)
+    shipping_rate = SHIPPING_PRICES.get(shipping_type)
+    #shipping_rate = SHIPPING_PRICES[shipping_type]
     price = distance * shipping_rate
     return format_price(price)
 
@@ -19,13 +19,28 @@ def calculate_driver_cost(distance, *drivers):
     cheapest_driver, cheapest_driver_price = None
     for driver in drivers:
         driver_time = driver.speed * distance
-        price_for_dirver = driver.salary * driver_time
+        price_for_driver = driver.salary * driver_time
         if cheapest_driver is None:
             cheapest_driver = driver
-            cheapest_driver_price = price_for_dirver
-        elif price_for_dirver < cheapest_driver_price:
+            cheapest_driver_price = price_for_driver
+        elif price_for_driver < cheapest_driver_price:
             cheapest_driver = driver
-            cheapest_driver_price = price_for_dirver
+            cheapest_driver_price = price_for_driver
     
     return cheapest_driver_price, cheapest_driver
 
+#Test
+test_function(calculate_driver_cost)
+
+#Nile Exclusive
+def calculate_money_made(**trips):
+    total_money_made = 0
+    for id, trip in trips.items():
+        trip_revenue = trip.cost - trip.driver.cost
+        total_money_made += trip_revenue
+    
+    return total_money_made
+
+
+#Test
+test_function(calculate_money_made)
