6adb3f34c529a1ca865047f485bcb597559dd25f
Chelsea <chelsea.e.rolland@gmail.com>
2021-03-23 14:34:22 -0500

    working on linked list DS and completed and somewhat understand the removal node process for linked list

--- further_learning/nodes_for_linked_lists.py
+++ further_learning/nodes_for_linked_lists.py
@@ -35,6 +35,20 @@ class LinkedList:
                 str_lst += str(current_node.get_value()) + "\n"
                 current_node = current_node.get_next_node()
         return str_lst
+    
+    def remove_node(self, value_to_remove):
+        current_node = self.get_head_node()
+        if current_node.get_value() == value_to_remove:
+            self.head_node = current_node.get_next_node()
+        else:
+            while current_node:
+                next_node = current_node.get_next_node()
+                if next_node.get_value() == value_to_remove:
+                    current_node.set_next_node(next_node.get_next_node())
+                    current_node = None
+                else:
+                    current_node = next_node
+
 
 #Testing the Linked List Case
 ll = LinkedList(5)
